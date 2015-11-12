2122bff91c022b5eac0f14a68d92042a87e57810
Julius Carvajal <juliuscarvajal21@gmail.com>
2015-11-13 10:13:55 +1100

    Added editing

--- controllers/app.js
+++ controllers/app.js
@@ -1,63 +1,120 @@
 angular.module('SB2')
-.controller('AppController', ['$scope', '$timeout', 'createChangeStream', 'LiveSet', function($scope, $timeout, createChangeStream, LiveSet) {	
+.controller('AppController', ['$scope', '$timeout', 'createChangeStream', 'LiveSet', function($scope, $timeout, createChangeStream, LiveSet) {
 
-	$scope.displayLimit = 5;
-	$scope.totalDisplayed = 0;
+  $scope.displayLimit = 5;
+  $scope.totalDisplayed = {};
 
-	var url = location.protocol + '//' + location.hostname + ':3000';
-	var apiUrl = url + '/api/v0';		
-	$scope.queryFilter =  function() {
-		return '?filter[limit]=' + $scope.displayLimit + '&filter[skip]=' + $scope.totalDisplayed;	
-	}
-	
+  var url = location.protocol + '//' + location.hostname + ':3000';
+  var apiUrl = url + '/api/v0';
+  var socket = io(url);
 
-	var eventSources = {};	
-	var changes = {};
-	function getModelData(model, filter, callback) {		
-		var model_plural = model + 's';
-		if (!eventSources[model]) {
-			eventSources[model] = new EventSource(apiUrl + '/' + model_plural + '/change-stream?_format=event-stream');			
-		 	changes[model] = createChangeStream(eventSources[model]);
-		}	
-			
-		var data = null;		
-		
-		$.ajax(apiUrl + '/' + model_plural + filter)
-		.done(function(result) {
-			$timeout(function() {				
-				data = new LiveSet(result, changes[model]);
-				$scope[model_plural] = data.toLiveArray();
-			}, 0);			
-		});	
-	};
-	
-	getModelData('Location', $scope.queryFilter());
-	getModelData('Channel', $scope.queryFilter());	
-				
-	$scope.done = function (model, field, newval, oldval) {
-		var data = {};
-		data[field] = newval[field]; 		 		
+  socket.on('connect', function() {
+    console.log('connected');
+  });
 
-		var model_plural = model + 's';	
-		$.ajax({
-			url: apiUrl + '/' + model_plural + '/' + newval.id,
-			type: 'PUT', // Use POST with X-HTTP-Method-Override or a straight PUT if appropriate.
-			dataType: 'json', // Set datatype - affects Accept header
-			data: data
-		})
-		.done(function() {
-			console.log('saved');	
-		});
-	};
-		
-	$scope.next = function(model) {
-		$scope.totalDisplayed += $scope.displayLimit;
-		getModelData(model, $scope.queryFilter());
-	};
-	
-	$scope.prev = function(model) {
-		$scope.totalDisplayed -= $scope.displayLimit;
-		getModelData(model, $scope.queryFilter());
-	};
-	
+  $scope.queryFilter = function(model) {
+    $scope.totalDisplayed[model] = $scope.totalDisplayed[model] || 0; //initialize if it does not exist.
+    return '?filter[limit]=' + $scope.displayLimit + '&filter[skip]=' + $scope.totalDisplayed[model];
+  };
+
+  var eventSources = {};
+  var changes = {};
+  function getModelData(model, filter, callback) {
+    var model_plural = model + 's';
+    if (!eventSources[model]) {
+      eventSources[model] = new EventSource(apiUrl + '/' + model_plural + '/change-stream?_format=event-stream');
+       changes[model] = createChangeStream(eventSources[model]);
+    }
+
+    var data = null;
+
+    $.ajax(apiUrl + '/' + model_plural + filter(model))
+    .done(function(result) {
+      $timeout(function() {
+        data = new LiveSet(result, changes[model]);
+        $scope[model_plural] = data.toLiveArray();
+      }, 0);
+    });
+  }
+
+  getModelData('Location', $scope.queryFilter);
+  getModelData('Channel', $scope.queryFilter);
+
+  $scope.done = function (model, field, newval, oldval) {
+    var data = {};
+    data[field] = newval[field];
+
+    var model_plural = model + 's';
+    $.ajax({
+      url: apiUrl + '/' + model_plural + '/' + newval.id,
+      type: 'PUT', // Use POST with X-HTTP-Method-Override or a straight PUT if appropriate.
+      dataType: 'json', // Set datatype - affects Accept header
+      data: data
+    })
+    .done(function() {
+      console.log('saved');
+    });
+  };
+
+  $scope.editing = function (model, field, data) {
+    socket.emit('coates_event', {
+      source: socket.id,
+      action: 'editing',
+      data: {
+        model: model,
+        id: data.id,
+        field: field
+      }
+    });
+  };
+
+  $scope.notEditing = function (model, field, data) {
+    $('#tr-' + data.id).removeClass('error');
+    socket.emit('coates_event', {
+      source: socket.id,
+      action: 'not_editing',
+      data: {
+        model: model,
+        id: data.id,
+        field: field
+      }
+    });
+  };
+
+  function toggleEditing(notification) {
+    if (!notification)
+      return;
+
+    var data = notification.data;
+    var editing = notification.action;
+    var source = notification.source;
+    if (editing === 'editing')
+      $('#tr-' + data.id).attr('class', 'error');
+    else
+      $('#tr-' + data.id).removeClass('error');
+  }
+
+  var lastNotification = null;
+  socket.on('coates_notify', function(notification) {
+    lastNotification = notification;
+    toggleEditing(notification);
+  });
+
+  $scope.hasPrev = function(model) {
+    return $scope.totalDisplayed[model] > 0;
+  }
+
+  $scope.next = function(model) {
+    $scope.totalDisplayed[model] += $scope.displayLimit;
+    getModelData(model, $scope.queryFilter);
+    toggleEditing(lastNotification);
+
+  };
+
+  $scope.prev = function(model) {
+    $scope.totalDisplayed[model] -= $scope.displayLimit;
+    getModelData(model, $scope.queryFilter);
+    toggleEditing(notification);
+  };
+
 }]);
